8d9d321a4e558ea50219f0a30651cbe34d7a7438
Saenko Dmitry <dmitry.saenko@aliexpress.ru>
2026-01-18 19:16:29 +0300

    fix migrations 2

--- db/migrations/20260110184146_create_table_exercises.sql
+++ db/migrations/20260110184146_create_table_exercises.sql
@@ -5,7 +5,6 @@ CREATE TABLE exercises
     id               BIGSERIAL PRIMARY KEY,
     workout_day_id   BIGINT NOT NULL REFERENCES workout_days (id) ON DELETE CASCADE,
     exercise_type_id BIGINT NOT NULL REFERENCES exercise_types (id),
-    rest_in_seconds  INT,
     index            BIGINT
 );
 -- +goose StatementEnd
