a60006617830ffeaba3651aa81ce3f547e0d7a8c
Saenko Dmitry <dmitry.saenko@aliexpress.ru>
2026-02-12 01:07:23 +0300

    change icons

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,35 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Form Journey · Training 🏔️',
+                short_name: 'Form Journey',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/web-app-manifest-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/web-app-manifest-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
     build: {
         outDir: "../internal/web/dist", // если embed
         emptyOutDir: true,
